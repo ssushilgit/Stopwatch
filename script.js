e3c7bdbc897fd62682dd92fb05d79138025d4073
ssushilgit <shresthasushil741@gmail.com>
2024-11-30 00:33:33 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayTime = document.getElementById('displayTime');
+
+function stopwatch(){
+    seconds++;
+    if(seconds==60){
+        seconds = 0
+        minutes++;
+        if(minutes==60){
+            minutes = 0
+            hours++;
+        }
+    }
+    function watchStart(){
+       if(timer!==null){
+            clearInterval(timer)
+       }
+        timer =  setInterval(stopwatch,1000); 
+    }
+     
+}
